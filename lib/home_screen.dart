22763ccab67ad6d57598e04da6b6fb8580a9af37
shauryagupta45 <shauryagupta4501@gmail.com>
2023-11-27 17:40:01 +0530

    update

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -10,6 +10,14 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Notes App"),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [],
+      ),
+    );
   }
 }
