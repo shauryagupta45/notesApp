13e1d01f3feaa60880fb54a34d3ead8f0a2524cf
shauryagupta45 <shauryagupta4501@gmail.com>
2023-12-02 20:41:57 +0530

    update

--- lib/db_handler.dart
+++ lib/db_handler.dart
@@ -33,4 +33,26 @@ class DbHelper {
     await dbClient!.insert('notes', notesModel.toMap());
     return notesModel;
   }
+
+  Future<List<NotesModel>> getNotesList() async {
+    var dbClient = await db;
+    final List<Map<String, Object?>> queryResult =
+        await dbClient!.query('notes');
+    return queryResult.map((e) => NotesModel.fromMap(e)).toList();
+  }
+
+  Future<int> delete(int id) async {
+    var dbClient = await db;
+    return await dbClient!.delete(
+      'notes',
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+  }
+
+  Future<int> update(NotesModel notesModel) async {
+    var dbClient = await db;
+    return await dbClient!.update('notes', notesModel.toMap(),
+        where: 'id =?', whereArgs: [notesModel.id]);
+  }
 }
--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -11,24 +11,89 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   DbHelper? dbHelper;
+  late Future<List<NotesModel>> notesList;
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
     dbHelper = DbHelper();
+    loadData();
+  }
+
+  loadData() async {
+    notesList = dbHelper!.getNotesList();
   }
 
   @override
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
-    double buttonHeight = 56.0;
     return Scaffold(
       appBar: AppBar(
         title: const Text("Notes App"),
         centerTitle: true,
       ),
       body: Column(
-        children: [],
+        children: [
+          Expanded(
+            child: FutureBuilder(
+                future: notesList,
+                builder: (context, AsyncSnapshot<List<NotesModel>> snapshot) {
+                  if (snapshot.hasData) {
+                    return ListView.builder(
+                        itemCount: snapshot.data?.length,
+                        reverse: false,
+                        shrinkWrap: true,
+                        itemBuilder: (context, index) {
+                          return InkWell(
+                            onTap: () {
+                              dbHelper!.update(
+                                NotesModel(
+                                  id: snapshot.data![index].id!,
+                                  title: 'Changed node',
+                                  age: 25,
+                                  description: 'the node has been changed',
+                                  email: 'abc@kimkmd',
+                                ),
+                              );
+                              setState(() {
+                                notesList = dbHelper!.getNotesList();
+                              });
+                            },
+                            child: Dismissible(
+                              background: Container(
+                                child: Icon(
+                                  Icons.delete,
+                                  color: Colors.red,
+                                ),
+                              ),
+                              onDismissed: (DismissDirection direction) {
+                                setState(() {
+                                  dbHelper!.delete(snapshot.data![index].id!);
+                                  notesList = dbHelper!.getNotesList();
+                                  snapshot.data!.remove(snapshot.data![index]);
+                                });
+                              },
+                              key: ValueKey<int>(snapshot.data![index].id!),
+                              child: Card(
+                                child: ListTile(
+                                  title: Text(
+                                      snapshot.data![index].title.toString()),
+                                  subtitle: Text(snapshot
+                                      .data![index].description
+                                      .toString()),
+                                  trailing: Text(
+                                      snapshot.data![index].age.toString()),
+                                ),
+                              ),
+                            ),
+                          );
+                        });
+                  } else {
+                    return Container();
+                  }
+                }),
+          ),
+        ],
       ),
       floatingActionButton: Stack(
         alignment: Alignment.bottomCenter,
@@ -40,7 +105,6 @@ class _HomeScreenState extends State<HomeScreen> {
               onPressed: () {
                 dbHelper!
                     .insert(NotesModel(
-                  id: 1,
                   title: 'First Note',
                   age: 22,
                   description: 'Shaurya Notes',
@@ -50,6 +114,9 @@ class _HomeScreenState extends State<HomeScreen> {
                   print('Data Stored');
                 }).onError((error, stackTrace) {
                   print(error.toString());
+                });
+                setState(() {
+                  notesList = dbHelper!.getNotesList();
                 });
               },
               child: const Icon(Icons.add),
